06f68c61720d2d33e7aa51448bd4bfe98a440f00
PratikChandB19 <pratikchandb19@gmail.com>
2026-04-13 11:48:22 +0545

    Added domain entities with relationship

---
+++ backend/JobPortal.Domain/Entities/Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Domain.Entities
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public int SeekerId {  get; set; }
+        public string Status { get; set; } = "Applied";
+        public DateTime AppliedAt { get; set; } = DateTime.UtcNow;
+
+        //Navigation
+        public Job Job { get; set; }
+        public User Seeker { get; set; }
+    }
+
+}
---
+++ backend/JobPortal/JobPortal.API/Controllers/JobController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JobController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetJobs()
+        {
+            return Ok("Jobs endpoint working");
+        }
+    }
+}
