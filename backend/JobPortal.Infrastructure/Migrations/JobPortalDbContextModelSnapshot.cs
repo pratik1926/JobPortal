bb307b58f0726721f8374d9d0ad7ec679ec9224f
PratikChandB19 <pratikchandb19@gmail.com>
2026-04-21 10:59:42 +0545

    Auth System Refresh token

--- backend/JobPortal.Infrastructure/Migrations/JobPortalDbContextModelSnapshot.cs
+++ backend/JobPortal.Infrastructure/Migrations/JobPortalDbContextModelSnapshot.cs
@@ -117,6 +117,12 @@ namespace JobPortal.Infrastructure.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
+                    b.Property<string>("RefreshToken")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime?>("RefreshTokenExpiryTime")
+                        .HasColumnType("datetime2");
+
                     b.Property<string>("Role")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
